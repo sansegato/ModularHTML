baa9032f64590a1d30da38cec4a4b92565501934
unknown <raphaelf@.(none)>
2012-01-19 17:46:05 -0200

    Teste (GitHub)

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
 ?>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript">
-//código renderizado
+// *código renderizado
 $(document).ready(function() {
 	$('.visualizacao').each(function() {
 		var render = $(this).html();
