7dab3e225e643ff5b71eea27d622b6c494a28afd
Adriano Ribeiro <adriano.ribeiro@gmail.com>
2012-01-20 19:10:02 -0200

    Inclusão do módulo 2.4.breadcrumbs

---
+++ modulos/2.4.breadcrumbs/index.php
@@ -0,0 +1,9 @@
+<nav id="breadcrumbs">
+	<ul>
+		<li>
+			<li class="inicio"><a href="/">In&iacute;cio</a></li>
+			<li><a href="#">Nome da se&ccedil;&atilde;o</a></li>
+			<li class="current">Nome da p&aacute;gina atual</li>
+		</li>
+	</ul>
+</nav>
