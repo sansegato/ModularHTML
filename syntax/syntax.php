b1b76be180316597421e6a4e772a5fb553e7aac2
raphaelfabeni <rapha.fabeni@gmail.com>
2012-01-20 14:01:11 -0200

    Instalando script SyntaxHighlighter para colorir o código mostrado.

---
+++ syntax/syntax.php
@@ -0,0 +1,13 @@
+<script type="text/javascript" src="syntax/scripts/shCore.js"></script>
+<script type="text/javascript" src="syntax/scripts/shBrushXml.js"></script>
+<link type="text/css" rel="stylesheet" href="syntax/styles/shCore.css"/>
+<link type="text/css" rel="stylesheet" href="syntax/styles/shThemeRDark.css"/>	
+<script type="text/javascript">
+	SyntaxHighlighter.config.clipboardSwf = 'syntax/scripts/clipboard.swf';
+	SyntaxHighlighter.all();
+	SyntaxHighlighter.config.strings.viewSource = "Ver código-fonte";
+	SyntaxHighlighter.config.strings.copyToClipboard = "Copiar para Área de Transferência";
+	SyntaxHighlighter.config.strings.copyToClipboardConfirmation = "Código copiado para Área de Transferência";
+	SyntaxHighlighter.config.strings.print = "Imprimir";
+	SyntaxHighlighter.config.strings.help = "Sobre";
+</script>e
