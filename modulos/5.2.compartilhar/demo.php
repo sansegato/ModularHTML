c2a3b68869cb4b765ff0361df3498073fb3659bc
Airton <airton.info@gmail.com>
2012-01-24 18:24:40 -0200

    Atualização no módulo 5.2.compartilhar

--- modulos/5.2.compartilhar/demo.php
+++ modulos/5.2.compartilhar/demo.php
@@ -14,41 +14,9 @@
 
 </head>
 <body>
-<div id="main">
-	<?php include ('index.php'); ?>
-</div>
-
-<!--//JS -->
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
-
-<script src="<?php echo ($_SERVER['DOCUMENT_ROOT']."/js/jquery.sharrre-1.2.0.min.js"); ?>"></script>
-
-<script>
-$('#shareme').sharrre({
-	share: {
-	googlePlus: true,
-	facebook: true,
-	twitter: true,
-	digg: true,
-	delicious: true,
-	stumbleupon: true,
-	linkedin: true
-	},
-	buttons: {
-	googlePlus: {size: 'tall'},
-	facebook: {layout: 'box_count'},
-	twitter: {count: 'vertical'},
-	digg: {type: 'DiggMedium'},
-	delicious: {size: 'tall'},
-	stumbleupon: {layout: '5'},
-	linkedin: {counter: 'top'}
-	},
-	enableHover: false,
-	enableCounter: false,
-	enableTracking: true
-});
-</script>
-<!--//JS -->
-
+	<div id="main">
+		<?php include ('index.php'); ?>
+	</div>
+	<?php include('../../global.js.php'); ?>
 </body>
 </html>
