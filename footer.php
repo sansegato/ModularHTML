62eac87294612ef7caa3d7c8f899cf9a219e123d
Adriano Ribeiro <adriano.ribeiro@gmail.com>
2012-01-24 13:33:04 -0200

    Assinatura no footer e ajustes visuais de CSS

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
 <footer>
-<p>?</p>
+	<p class="sobre"><span>ModularHTML</span> é um projeto <a href="https://github.com/a2comunicacao/ModularHTML" lang="en">open source</a> criado e mantido pela equipe de Design da <a href="http://www.a2comunicacao.com.br/">A2 Comunicação</a></p>
+	<p id="avatares">		
+		<a href="http://twitter.com/adriano"><img src="http://api.twitter.com/1/users/profile_image?screen_name=adriano&size=normal" alt="Adriano Ribeiro"></a>
+		<a href="http://twitter.com/airtonvancin"><img src="http://api.twitter.com/1/users/profile_image?screen_name=airtonvancin&size=normal" alt="Airton Vancin"></a>
+		<a href="http://twitter.com/raphaelfabeni"><img src="http://api.twitter.com/1/users/profile_image?screen_name=raphaelfabeni&size=normal" alt="Raphael Fabeni"></a>
+		<a href="http://twitter.com/drigoteixeira"><img src="http://api.twitter.com/1/users/profile_image?screen_name=drigoteixeira&size=normal" alt="Rodrigo Teixeira"></a>
+		<a href="http://twitter.com/sansegato"><img src="http://api.twitter.com/1/users/profile_image?screen_name=sansegato&size=normal" alt="Sandra Segato"></a>
+		<a href="http://twitter.com/viiholiveira"><img src="http://api.twitter.com/1/users/profile_image?screen_name=viiholiveira&size=normal" alt="Vivian Oliveira"></a>
+	</p>		
 </footer>
