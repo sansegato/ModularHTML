22dfa1bef11bb1beb761e90e61eda5ccf228e761
Adriano Ribeiro <adriano.ribeiro@gmail.com>
2012-01-26 14:18:57 -0200

    Inseri o código do Google Analytics

--- index.php
+++ index.php
@@ -46,6 +46,17 @@ $('#shareme').sharrre({
 </script>
 <!--@5.02.compartilhar -->
 
+<script type="text/javascript">
+ var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-23940018-5']);
+ _gaq.push(['_trackPageview']);
+
+ (function() {
+   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
+</script>
 
 </body>
 </html>
