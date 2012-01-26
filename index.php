c5642738f39ef1576aaac24de2c4c3746f67c2da
Adriano Ribeiro <adriano.ribeiro@gmail.com>
2012-01-26 14:17:24 -0200

    Botões de compartilhar no index.php e ajustes de CSS no footer

--- index.php
+++ index.php
@@ -17,10 +17,35 @@
 		<li><a href="demo.php">Demonstra&ccedil;&atilde;o</a></li>
 		<li><a href="https://github.com/a2comunicacao/ModularHTML">Download</a></li>
 	</ul>
+	<div class="social-networks-share">
+		<div id="shareme" data-url="http://modularhtml.com.br/" data-text="Make your sharing widget with Sharrre (jQuery Plugin)"></div>
+	</div>
 </div>
 
 <?php include ('footer.php'); ?>
 
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<!--@5.02.compartilhar -->
+<script src="/js/modulos/jquery.sharrre-1.2.0.min.js"></script>
+<script>
+$('#shareme').sharrre({
+	share: {
+	googlePlus: true,
+	facebook: true,
+	twitter: true
+	},
+	buttons: {
+	googlePlus: {size: 'small', annotation: 'none' },
+	facebook: {layout: 'button_count'},
+	twitter: {count: 'none' }
+	},
+	enableHover: false,
+	enableCounter: false,
+	enableTracking: true
+});
+</script>
+<!--@5.02.compartilhar -->
+
+
 </body>
 </html>
