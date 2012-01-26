920a748a9017d1e9b68c79253e905cd8f1961fd4
raphaelfabeni <rapha.fabeni@gmail.com>
2012-01-26 11:02:30 -0200

    Novo módulo adicionado : 6.3.lista-de-artigos

--- modulos/3.4.data/index.php
+++ modulos/3.4.data/index.php
@@ -1,1 +1,3 @@
-<p class="date"><abbr title="2012-01-19">19/01/2012</abbr></p>
+<p class="date">
+	<abbr title="2012-01-19">19/01/2012</abbr>
+</p>
---
+++ modulos/6.3.lista-de-artigos/index.php
@@ -0,0 +1,14 @@
+<ul class="list-news">
+	<li>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/3.4.data/index.php');?>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/4.4.thumbnails/index.php');?>
+		<div class="content-type">
+			<figure><img src="/modulos/6.3.lista-de-artigos/img/archive.png" alt="Arquivo" /></figure>
+			<figure><img src="/modulos/6.3.lista-de-artigos/img/sound.png" alt="Áudio" /></figure>
+			<figure><img src="/modulos/6.3.lista-de-artigos/img/picture.png" alt="Imagem" /></figure>
+			<figure><img src="/modulos/6.3.lista-de-artigos/img/film.png" alt="Vídeo" /></figure>
+		</div>
+		<h3><a href="#">Lorem ipsum dolor sit amet</a></h3>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/3.2.subtitulo/index.php');?>
+	</li>
+</ul>
