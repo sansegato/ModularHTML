b831d9f0d400b09f4746fece6070474b5c3d519c
Airton <airton.info@gmail.com>
2012-01-26 12:29:09 -0200

    Adicionado módulo 2.8.nuvem-de-tags

--- global.js.php
+++ global.js.php
@@ -1,5 +1,27 @@
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 
+
+<!--@2.8.nuvem-de-tags -->
+<!--[if lt IE 9]><script type="text/javascript" src="/js/modulos/excanvas.js"></script><![endif]-->
+<script src="/js/modulos/jquery.tagcanvas.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	if(!$('#myCanvas').tagcanvas({
+	  textColour: '#666',
+	  outlineColour: '#bbb',
+	  reverse: true,
+	  depth: 0.8,
+	  maxSpeed: 0.05,
+	  zoom: 1.2,
+	  zoomStep: 0
+	},'tag-cloud')) {
+	  // something went wrong, hide the canvas container
+	  $('#myCanvasContainer').hide();
+	}
+});
+</script>
+<!--@2.8.nuvem-de-tags -->
+	
 <!--@5.2.compartilhar -->
 <script src="/js/modulos/jquery.sharrre-1.2.0.min.js"></script>
 <script>
