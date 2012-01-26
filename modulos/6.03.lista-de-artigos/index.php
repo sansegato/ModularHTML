eb883d9ba68a5ce9bf43c0a14951ba0e056584d8
raphaelfabeni <rapha.fabeni@gmail.com>
2012-01-26 11:15:36 -0200

    Ajustes no padrão de nomenclatura dos Módulos em virtude da ordem alfabética

---
+++ modulos/6.03.lista-de-artigos/index.php
@@ -0,0 +1,14 @@
+<ul class="list-news">
+	<li>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/3.04.data/index.php');?>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/4.04.thumbnails/index.php');?>
+		<div class="content-type">
+			<figure><img src="/modulos/6.03.lista-de-artigos/img/archive.png" alt="Arquivo" /></figure>
+			<figure><img src="/modulos/6.03.lista-de-artigos/img/sound.png" alt="Áudio" /></figure>
+			<figure><img src="/modulos/6.03.lista-de-artigos/img/picture.png" alt="Imagem" /></figure>
+			<figure><img src="/modulos/6.03.lista-de-artigos/img/film.png" alt="Vídeo" /></figure>
+		</div>
+		<h3><a href="#">Lorem ipsum dolor sit amet</a></h3>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/3.02.subtitulo/index.php');?>
+	</li>
+</ul>
