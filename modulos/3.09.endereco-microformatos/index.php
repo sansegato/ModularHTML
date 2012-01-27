e7b46e09d731ff88411728a357d3c03f2f58f427
raphaelfabeni <rapha.fabeni@gmail.com>
2012-01-27 16:20:47 -0200

    Módulo adicionado: 3.09.endereço-microformatos

---
+++ modulos/3.09.endereco-microformatos/index.php
@@ -0,0 +1,14 @@
+<address class="vcard">	
+	<span class="title">
+		<span class="copyright">© 2012</span>
+		<span class="org fn">A2 Comunicação</span>
+	</span>
+	<span class="adr">
+		<span class="street-address">R. Doutor Costa Júnior, 465</span> -
+		CEP <span class="postal-code">05002-000</span> - 
+		<span class="locality">São Paulo</span> -
+		<abbr title="São Paulo" class="region">SP</abbr> -
+		<span class="country-name">Brasil</span> - 
+		<span class="tel">55 (11) 3872-5565</span>
+	</span>			
+</address>
