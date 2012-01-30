a1f798a1d5999bc2a8fae2243b591269a67e8545
raphaelfabeni <rapha.fabeni@gmail.com>
2012-01-30 16:02:12 -0200

    Novo módulo adicionado: 2.07.accordion

---
+++ modulos/2.07.accordion/index.php
@@ -0,0 +1,26 @@
+<div class="accordion">
+	<div class="item">
+		<h3><a href="#">Item 1</a></h3>
+		<div class="item-content">
+			<p>Reflexionis suo aperuit in deinde cepit roseo commendavit auri. Stranguillione in rei completo litus vita Apolloni figitur acquievit. Discumbere acies regina in rei sensibilium iussit est cum autem Apolloni dicere nec benedictus filiae gloriam virginis instaret. Proiciens te sed esse ait regem Boreas quam dicentes parem. Triton testandum ecce prima inrumpit dic enim est amet Cur meae in modo genito in modo genito in.</p>
+		</div>
+	</div>
+	<div class="item">
+		<h3><a href="#">Item 2</a></h3>
+		<div class="item-content">
+			<p>Reflexionis suo aperuit in deinde cepit roseo commendavit auri. Stranguillione in rei completo litus vita Apolloni figitur acquievit. Discumbere acies regina in rei sensibilium iussit est cum autem Apolloni dicere nec benedictus filiae gloriam virginis instaret. Proiciens te sed esse ait regem Boreas quam dicentes parem. Triton testandum ecce prima inrumpit dic enim est amet Cur meae in modo genito in modo genito in.</p>
+		</div>
+	</div>
+	<div class="item">
+		<h3><a href="#">Item 3</a></h3>
+		<div class="item-content">
+			<p>Reflexionis suo aperuit in deinde cepit roseo commendavit auri. Stranguillione in rei completo litus vita Apolloni figitur acquievit. Discumbere acies regina in rei sensibilium iussit est cum autem Apolloni dicere nec benedictus filiae gloriam virginis instaret. Proiciens te sed esse ait regem Boreas quam dicentes parem. Triton testandum ecce prima inrumpit dic enim est amet Cur meae in modo genito in modo genito in.</p>
+		</div>
+	</div>
+	<div class="item">
+		<h3><a href="#">Item 4</a></h3>
+		<div class="item-content">
+			<p>Reflexionis suo aperuit in deinde cepit roseo commendavit auri. Stranguillione in rei completo litus vita Apolloni figitur acquievit. Discumbere acies regina in rei sensibilium iussit est cum autem Apolloni dicere nec benedictus filiae gloriam virginis instaret. Proiciens te sed esse ait regem Boreas quam dicentes parem. Triton testandum ecce prima inrumpit dic enim est amet Cur meae in modo genito in modo genito in.</p>
+		</div>
+	</div>
+</div>
