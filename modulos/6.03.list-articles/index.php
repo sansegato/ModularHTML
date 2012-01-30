ba6d5041da4e3eae1934309aae91f9593eeb67cf
Vivian Oliveira <vivinhaa@gmail.com>
2012-01-30 16:56:00 -0200

    Tradução dos módulos e códigos para inglês

---
+++ modulos/6.03.list-articles/index.php
@@ -0,0 +1,14 @@
+<ul class="list-articles">
+	<li>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/3.04.date/index.php');?>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/4.04.thumbnails/index.php');?>
+		<div class="content-type">
+			<figure><img src="/modulos/6.03.list-articles/img/archive.png" alt="Arquivo" /></figure>
+			<figure><img src="/modulos/6.03.list-articles/img/sound.png" alt="Áudio" /></figure>
+			<figure><img src="/modulos/6.03.list-articles/img/picture.png" alt="Imagem" /></figure>
+			<figure><img src="/modulos/6.03.list-articles/img/film.png" alt="Vídeo" /></figure>
+		</div>
+		<h3><a href="#">Lorem ipsum dolor sit amet</a></h3>
+		<?php include($_SERVER['DOCUMENT_ROOT'].'/modulos/3.02.subtitle/index.php');?>
+	</li>
+</ul>
