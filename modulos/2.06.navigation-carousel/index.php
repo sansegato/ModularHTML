a9ecef0d7a5d9c52883db5c79ac1dff769a32afb
raphaelfabeni <rapha.fabeni@gmail.com>
2012-02-01 13:40:07 -0200

    Módulo adicionado: 2.06.navigation-carousel

---
+++ modulos/2.06.navigation-carousel/index.php
@@ -0,0 +1,23 @@
+<div id="carousel" class="es-carousel-wrapper">
+	<div class="es-carousel">
+		<ul>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+			<li><a href="#"><img src="http://placehold.it/90x90" alt="Imagem" /></a></li>
+		</ul>
+	</div>
+</div>
