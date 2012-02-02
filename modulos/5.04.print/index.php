b7fb0fe42192a804a993e9123a17f2fc696fe19b
raphaelfabeni <rapha.fabeni@gmail.com>
2012-02-02 12:07:52 -0200

    Ajustes no Módulo 8.09.form-selection-action | Novos módulos adicionados: 5.03.control-font-size e 5.04.print

---
+++ modulos/5.04.print/index.php
@@ -0,0 +1,5 @@
+<div class="print-text">
+	<a href="#" id="print">Imprimir</a>
+</div>
+
+<p>Lorem ipsum dolor sit amet, casus adprehendens melius ait mea ego Pentapolim Cyrenaeorum tertia veni est se est Apollonius. Ipso mihi cum magna duobus auro fulgida, ite in lucem concitaverunt in rei sensibilium. Quaestiones patre nihil civitatis in rei exultant deo hanc, descendit rationis ad suis alteri formam cum autem nobiscum. Mansione manere praeparabant dicam deus eum, regem adventu nihil labore in lucem. Tharsia Theophilo litore est Apollonius eius in lucem genero ergo accipiet.</p>
